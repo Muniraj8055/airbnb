d2b0139e68b08b2f5fcd04fc885adcdd55adb454
Muniraj8055 <muniraj150299@gmail.com>
2023-06-06 22:37:28 +0530

    my first comit

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Header from '../Header'
+
+function IndexPage() {
+  return (
+    <div>
+       <Header/>  
+    </div>
+  )
+}
+
+export default IndexPage
